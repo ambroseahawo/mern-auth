a4d9ece07cf1c44096d725b630a88d1f3ebce290
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-12-17 19:29:28 +0300

    Manage environment variables

---
+++ src/config/app.config.ts
@@ -0,0 +1,20 @@
+import { getEnv } from "@/common/utils/get-env";
+import "dotenv/config";
+
+const appConfig = () => ({
+  NODE_ENV: getEnv("NODE_ENV"),
+  APP_ORIGIN: getEnv("APP_ORIGIN"),
+  PORT: getEnv("PORT"),
+  BASE_PATH: getEnv("BASE_PATH"),
+  MONGO_URI: getEnv("MONGO_URI"),
+  JWT: {
+    SECRET: getEnv("JWT_SECRET"),
+    EXPIRES_IN: getEnv("JWT_EXPIRES_IN"),
+    REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET"),
+    REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN"),
+  },
+  MAILER_SENDER: getEnv("MAILER_SENDER"),
+  RESEND_API_KEY: getEnv("RESEND_API_KEY"),
+});
+
+export const config = appConfig();
