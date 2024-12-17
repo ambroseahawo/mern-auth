d643bbfc1d5a601512bde6a9e0213f5fcd97f49f
Ambrose Ahawo <ambroseahawo@gmail.com>
2024-12-17 19:28:53 +0300

    Implement index entry file

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+require("module-alias/register");
+
+import { config } from "@/config/app.config";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: config.APP_ORIGIN, credentials: true }));
+app.use(cookieParser());
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).json({
+    message: "mern-auth",
+  });
+});
+app.listen(config.PORT, async () => {
+  console.log(`Server listening on port ${config.PORT} in ${config.NODE_ENV}`);
+});
