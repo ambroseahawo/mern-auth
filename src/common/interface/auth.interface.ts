ee9294789f2c4bc0695aa179fc84d584dd93b8a3
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-15 23:25:30 +0300

    Implement auth interface

---
+++ src/common/interface/auth.interface.ts
@@ -0,0 +1,18 @@
+export interface RegisterDto {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  userAgent?: string;
+}
+
+export interface LoginDto {
+  email: string;
+  password: string;
+  userAgent?: string;
+}
+
+export interface ResetPasswordDto {
+  password: string;
+  verificationCode: string;
+}
