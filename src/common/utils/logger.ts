70733c8eb3b60ddca0b7d3995e0bc9c04c839502
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-16 20:38:28 +0300

    Implement logger util

---
+++ src/common/utils/logger.ts
@@ -0,0 +1,25 @@
+import { config } from "@/config/app.config";
+import { createLogger, format, transports } from "winston";
+import "winston-daily-rotate-file";
+
+const logFormat = format.combine(
+  format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  format.printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+  }),
+);
+
+export const logger = createLogger({
+  level: config.NODE_ENV === "production" ? "info" : "debug",
+  format: logFormat,
+  transports: [
+    new transports.Console(),
+    new transports.DailyRotateFile({
+      dirname: "logs",
+      filename: "%DATE%-app.log",
+      datePattern: "YYYY-MM-DD",
+      maxSize: "20m",
+      maxFiles: "14d",
+    }),
+  ],
+});
