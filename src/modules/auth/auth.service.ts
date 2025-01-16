d40aa7e0f8eb054405c17ec5bea16495bbe0f4d5
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-17 02:00:26 +0300

    Implement auth service verify email

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -113,4 +113,27 @@ export class AuthService {
 
     return { accessToken, newRefreshToken };
   }
+
+  public async verifyEmail(code: string) {
+    const validCode = await VerificationCodeModel.findOne({
+      code: code,
+      type: VerificationEnum.EMAIL_VERIFICATION,
+      expiresAt: { $gt: new Date() },
+    });
+    if (!validCode) {
+      throw new BadRequestException("Invalid or Expired verification code");
+    }
+
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      validCode.userId,
+      { isEmailVerified: true },
+      { new: true },
+    );
+    if (!updatedUser) {
+      throw new BadRequestException("Unable to verify email address", ErrorCode.VALIDATION_ERROR);
+    }
+
+    await validCode.deleteOne();
+    return { user: updatedUser };
+  }
 }
