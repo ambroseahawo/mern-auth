0ad718947751d36db3828e8c8854b318d28259c5
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-17 15:38:47 +0300

    Refactor send mail

--- src/mailers/mailers.ts
+++ src/mailers/mailers.ts
@@ -1,5 +1,6 @@
 import { config } from "@/config/app.config";
 import { resend } from "@/mailers/resendClient";
+import { CreateEmailResponse as ResendCreateEmailResponse } from "resend";
 
 type Params = {
   to: string | string[];
@@ -9,11 +10,33 @@ type Params = {
   from?: string;
 };
 
+interface CreateEmailResponse extends ResendCreateEmailResponse {
+  id: string;
+}
+
 const mailer_sender =
   config.NODE_ENV === "development"
     ? "no-reply <onboarding@resend.dev>"
     : `no-reply <${config.MAILER_SENDER}>`;
 
-export const sendEmail = async ({ to, from = mailer_sender, subject, text, html }: Params) => {
-  await resend.emails.send({ from, to: Array.isArray(to) ? to : [to], text, subject, html });
+export const sendEmail = async ({
+  to,
+  from = mailer_sender,
+  subject,
+  text,
+  html,
+}: Params): Promise<{ data?: CreateEmailResponse; error?: any }> => {
+  try {
+    const data = (await resend.emails.send({
+      from,
+      to: Array.isArray(to) ? to : [to],
+      text,
+      subject,
+      html,
+    })) as CreateEmailResponse;
+
+    return { data };
+  } catch (error: any) {
+    return { error };
+  }
 };
