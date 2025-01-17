abc0142149c201c467fff19ae08d76e40d8095ec
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-17 23:36:39 +0300

    Implement auth controller refresh token

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { setAuthenticationCookies } from "@/common/utils/cookie";
+import { UnauthorizedException } from "@/common/utils/catch-errors";
+import {
+  getAccessTokenCookieOptions,
+  getRefreshTokenCookieOptions,
+  setAuthenticationCookies,
+} from "@/common/utils/cookie";
 import { loginSchema, registerSchema } from "@/common/validators/auth.validator";
 import { HTTPSTATUS } from "@/config/http.config";
 import { asyncHandler } from "@/middlewares/asyncHandler";
@@ -32,10 +37,33 @@ export class AuthController {
       });
     }
 
-    return setAuthenticationCookies({ res, accessToken, refreshToken }).status(HTTPSTATUS.OK).json({
-      message: "User logged in successfully",
-      mfaRequired,
-      user,
-    });
+    return setAuthenticationCookies({
+      res,
+      accessToken,
+      refreshToken,
+    })
+      .status(HTTPSTATUS.OK)
+      .json({
+        message: "User logged in successfully",
+        mfaRequired,
+        user,
+      });
+  });
+
+  public refreshToken = asyncHandler(async (req: Request, res: Response): Promise<any> => {
+    const refreshToken = req.cookies.refreshToken as string | undefined;
+    if (!refreshToken) {
+      throw new UnauthorizedException("Missing refresh token");
+    }
+
+    const { accessToken, newRefreshToken } = await this.authService.refreshToken(refreshToken);
+    if (newRefreshToken) {
+      res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions());
+    }
+
+    return res
+      .status(HTTPSTATUS.OK)
+      .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+      .json({ message: "Refresh access token successfully" });
   });
 }
