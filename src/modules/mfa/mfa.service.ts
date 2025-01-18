1272f9825e0d1d534bf56f72e3c1fd8f225a12e2
Ambrose Ahawo <ambroseahawo@gmail.com>
2025-01-18 13:02:22 +0300

    Implement mfa service verify mfa setup

--- src/modules/mfa/mfa.service.ts
+++ src/modules/mfa/mfa.service.ts
@@ -1,4 +1,4 @@
-import { UnauthorizedException } from "@/common/utils/catch-errors";
+import { BadRequestException, UnauthorizedException } from "@/common/utils/catch-errors";
 import { Request } from "express";
 import qrcode from "qrcode";
 import speakeasy from "speakeasy";
@@ -32,5 +32,39 @@ export class MfaService {
 
     const qrImageUrl = await qrcode.toDataURL(url);
     return { message: "Scan the QR code or use the setup key", secret: secretKey, qrImageUrl };
+  }
+
+  public async verifyMFASetup(req: Request, code: string, secretKey: string) {
+    const user = req.user;
+    if (!user) {
+      throw new UnauthorizedException("User not authorized");
+    }
+
+    if (user.userPreferences.enable2FA) {
+      return {
+        message: "MFA already enabled",
+        userPreferences: { enable2FA: user.userPreferences.enable2FA },
+      };
+    }
+
+    const isValid = speakeasy.totp.verify({
+      secret: secretKey,
+      encoding: "base32",
+      token: code,
+    });
+
+    if (!isValid) {
+      throw new BadRequestException("Invalid MFA code. Please try again");
+    }
+
+    user.userPreferences.enable2FA = true;
+    await user.save();
+
+    return {
+      message: "MFA setup completed successfully",
+      userPreferences: {
+        enable2FA: user.userPreferences.enable2FA,
+      },
+    };
   }
 }
